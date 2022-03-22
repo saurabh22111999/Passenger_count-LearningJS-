157551cf4aa9b473e56fc960a5c5b5e0bea9b003
Saurabh Jangra <saurbhjangra111@gmail.com>
2022-03-23 03:48:46 +0530

    js

---
+++ PassengerCountApp.js
@@ -0,0 +1,9 @@
+// document.getElementById("count-el").innerText=5
+
+ let count =0;
+ let countel= document.getElementById("count-el")
+ console.log(countel)
+function increment(){
+count=count+1;
+countel.innerText=count;
+}
