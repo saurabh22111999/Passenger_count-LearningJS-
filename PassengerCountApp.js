27ab26d36d50973149121dacb74e3dbf8fa46dbd
Saurabh Jangra <saurbhjangra111@gmail.com>
2022-03-26 03:20:07 +0530

    app completed

--- PassengerCountApp.js
+++ PassengerCountApp.js
@@ -4,9 +4,17 @@
  let countel= document.getElementById("count-el")
  console.log(countel)
 function increment(){
-count=count+1;
+count+=1;
 countel.innerText=count;
 }
+let saveEl=document.getElementById("saveEl");
 function save(){
     console.log(count);
-}
+    let countstr= count+" - ";
+  
+    saveEl.textContent+=countstr;
+    // innerText sometimes struggles to return things that are hidden or not human readable
+    //also cant read space and more expensive in computauion
+    countel.textContent=0;
+count=0;
+} 
